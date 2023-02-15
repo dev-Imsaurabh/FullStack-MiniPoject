5210a6f17ad9674d40fb589771e09b0aaceefdf0
dev-Iamsaurabh <sau4478@gmail.com>
2023-02-15 18:31:52 +0530

    Fullstack mern project

---
+++ Backend/index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const { connection } = require("./db")
+const { authenticator } = require("./middlewares/authenticator")
+const { roleValidator } = require("./middlewares/roleValidator")
+const { userLogger } = require("./middlewares/userLogger")
+const { userValidator } = require("./middlewares/uservalidator")
+const { userRouter } = require("./routes/user.routes")
+
+require("dotenv").config()
+const port = process.env.PORT
+
+const app = express()
+app.use(express.json())
+app.use(userValidator)
+app.use(authenticator)
+app.use(userLogger)
+app.use("/user",roleValidator)
+
+app.use("/user",userRouter)
+
+
+app.listen(port,async()=>{
+
+    try {
+        await connection
+        console.log("Connected to db")
+    } catch (error) {
+        console.log(error)
+    }
+
+
+    console.log("Server is started on port number",port)
+})
